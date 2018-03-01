c93f9d3f5ab97a3f04e812cbae233b302c03b663
daivscott <daivgit@mail.com>
2018-03-01 23:15:48 +0000

    Shooting and weapon mechanic
    Added shooting and weapon mechanics.  Initial setting of player tags/layer

---
+++ TheLight/Assets/PlayerShoot.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    private const string PLAYER_TAG = "PLayer";
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    
+    void Start()
+    {
+        if (cam == null)
+        {  
+            Debug.LogError("PlayerShoot: No Camera referenced");
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot()
+    {
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        {
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(_hit.collider.name);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot (string _ID)
+    {
+        Debug.Log(_ID + " has been shot.");
+    }
+}
--- TheLight/Assets/Scripts/PlayerSetup.cs
+++ TheLight/Assets/Scripts/PlayerSetup.cs
@@ -7,23 +7,47 @@ public class PlayerSetup : NetworkBehaviour {
     [SerializeField]
     Behaviour[] componentsToDisable;
 
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
     Camera sceneCamera;
 
     void Start()
     {
         if (!isLocalPlayer)
         {
-            for(int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
-        } else
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
         {
             sceneCamera = Camera.main;
             if (sceneCamera != null)
             {
                 sceneCamera.gameObject.SetActive(false);
             }
+        }
+
+        RegisterPlayer();
+
+    }
+
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents ()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
         }
     }
 
