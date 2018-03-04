4f3de667738f7b00ff630284a7ba16accd009164
daivscott <daivgit@mail.com>
2018-03-04 05:27:09 +0000

    Dying and Respawning
    Added death and respawn functionalty

--- TheLight/Assets/Scripts/GameManager.cs
+++ TheLight/Assets/Scripts/GameManager.cs
@@ -4,6 +4,23 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance;
+
+    public MatchSettings matchSettings;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("More than one game manager in scene.");
+        } else
+        {
+            instance = this;
+        }
+    }
+
+    #region Player tracking
+
     private const string PLAYER_ID_PREFIX = "Player ";
 
     private static Dictionary<string, Player> players = new Dictionary<string, Player>();
@@ -38,4 +55,7 @@ public class GameManager : MonoBehaviour {
     //    GUILayout.EndVertical();
     //    GUILayout.EndArea();
     //}
+
+    #endregion
+
 }
