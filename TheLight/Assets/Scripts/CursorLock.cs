56a46694bbc6b91378bacd0f9c2da4ed67682f56
daivscott <daivgit@mail.com>
2018-03-09 11:36:06 +0000

    Cursor Lock and Crosshair
    Implemented temp cursor lock by pressing "1" to lock and "2 "to unlock. Also added a 2d texture for the crosshair.

---
+++ TheLight/Assets/Scripts/CursorLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("1"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+}
